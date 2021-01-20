8184c3bafa80fc95c47e65caf116b3de890f926f
jmchen <jmc1625645320@gmail.com>
2021-01-21 00:41:30 +0800

    v1.0.4 update

--- src/com/entity/Test.java
+++
@@ -1,9 +0,0 @@
-package com.entity;
-
-public class Test {
-    public static void main(String[] args) {
-        String a = "23";
-        int b = Integer.parseInt(a);
-        System.out.println(b);
-    }
-}
